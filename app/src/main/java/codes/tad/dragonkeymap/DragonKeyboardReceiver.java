a770962d6f7eceffcdd3ff60254c019c249ba1ea
Tad Fisher <tadfisher@gmail.com>
2016-04-16 14:13:07 -0700

    Initial commit.

---
+++ app/src/main/java/codes/tad/dragonkeymap/DragonKeyboardReceiver.java
@@ -0,0 +1,12 @@
+package codes.tad.dragonkeymap;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class DragonKeyboardReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // This is never called.
+    }
+}
